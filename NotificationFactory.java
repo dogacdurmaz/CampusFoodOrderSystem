0df75fbd561bc7808d8a9654d0dd926a73a59ee1
dogacdurmaz <dogacdurmaz@stu.aydin.edu.tr>
2026-05-14 17:31:54 +0300

    Create NotificationFactory.java

---
+++ NotificationFactory.java
@@ -0,0 +1,19 @@
+public class NotificationFactory {
+
+    public static Notification createNotification(String type) {
+
+        if (type.equalsIgnoreCase("EMAIL")) {
+            return new EmailNotification();
+        }
+
+        else if (type.equalsIgnoreCase("SMS")) {
+            return new SmsNotification();
+        }
+
+        else if (type.equalsIgnoreCase("PUSH")) {
+            return new PushNotification();
+        }
+
+        return null;
+    }
+}
